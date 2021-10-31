5e46328db7fcacdece29895fbfa5e7cdcdc7c0a0
asus <Asma-Almahdawi>
2021-10-31 17:21:27 +0300

    thank you

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -8,17 +8,17 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var falseButton:Button
+   private lateinit var falseButtonnnnn:Button
    private lateinit var trueButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        falseButton = findViewById(R.id.false_button)
+        falseButtonnnnn = findViewById(R.id.false_button)
         trueButton = findViewById(R.id.true_button)
 
-        falseButton.setOnClickListener {
+        falseButtonnnnn.setOnClickListener {
             val toast=  Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_LONG)
             toast.setGravity(Gravity.TOP,20,20)
             toast.show()
